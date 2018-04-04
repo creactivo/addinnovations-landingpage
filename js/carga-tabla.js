31a96fe40a02fb9690ab6ece9e6d13cb63be7ff5
Creactivo <zonacreactiva@gmail.com>
2018-04-04 18:14:22 -0500

    mas cambios

--- js/carga-tabla.js
+++ js/carga-tabla.js
@@ -1,40 +1,60 @@
-var aEmail =[],
-	aPassword =[],
-	aAddress =[];
+
+var camposList = [];
+
+function addCamposSistem(aip,anombre,aapellido,adireccion,aemail) {
+
+	var newCampos = {
+		ip: aip,
+		nombre: anombre,
+		apellido: aapellido,
+		direccion: adireccion,
+		email:aemail
+	};
+
+camposList.push(newCampos);
+
+localStorage.setItem("newCamposset", JSON.stringify(newCampos));
+
+
+}
+
+// var aEmail =[],
+// 	aPassword =[],
+// 	aAddress =[];
 
 
 
-	if(localStorage.getItem('lemail')!= null){
-		aEmail = JSON.parse(localStorage.getItem('lemail'));
-		aPassword = JSON.parse(localStorage.getItem('lpassword'));
-		aAddress = JSON.parse(localStorage.getItem('laddress'));
-	}
+// 	if(localStorage.getItem('lemail')!= null){
+// 		aEmail = JSON.parse(localStorage.getItem('lemail'));
+// 		aPassword = JSON.parse(localStorage.getItem('lpassword'));
+// 		aAddress = JSON.parse(localStorage.getItem('laddress'));
+// 	}
 
 
 
 
 
 
-var elementoBotonRegistrar = document.querySelector('#btn-enviar');
+// var elementoBotonRegistrar = document.querySelector('#btn-enviar');
 
-	elementoBotonRegistrar.addEventListener('click', registrarLibro);
-
-
-function registrarLibro() {
-	var bEmail = document.querySelector('#inputEmail4').value,
-		bPassword = document.querySelector('#inputPassword4').value,
-		bAddress = document.querySelector('#inputAddress').value;
-
-	aEmail.push(bEmail);
-	aPassword.push(bPassword);
-	aAddress.push(bAddress);
+// 	elementoBotonRegistrar.addEventListener('click', registrarLibro);
 
 
-	localStorage.setItem('lemail', JSON.stringify(aEmail));
-	localStorage.setItem('lpassword', JSON.stringify(aPassword));
-	localStorage.setItem('laddress', JSON.stringify(aAddress));
+// function registrarLibro() {
+// 	var bEmail = document.querySelector('#inputEmail4').value,
+// 		bPassword = document.querySelector('#inputPassword4').value,
+// 		bAddress = document.querySelector('#inputAddress').value;
+
+// 	aEmail.push(bEmail);
+// 	aPassword.push(bPassword);
+// 	aAddress.push(bAddress);
+
+
+// 	localStorage.setItem('lemail', JSON.stringify(aEmail));
+// 	localStorage.setItem('lpassword', JSON.stringify(aPassword));
+// 	localStorage.setItem('laddress', JSON.stringify(aAddress));
 
 
 
-};
+// };
 
--- js/mostrar-datos.js
+++ js/mostrar-datos.js
@@ -1,47 +1,88 @@
 
-llenarTabla();
+document.querySelector('#btn-enviar').addEventListener('click',salvarCampo);
+
+impTablaCampos()
+
+function salvarCampo(){
+	var sIp = document.querySelector('#textIp').value,
+		sNombre = document.querySelector('#textNombre').value,
+		sApellido = document.querySelector('#textApellido').value,
+		sDireccion = document.querySelector('#textDireccion').value,
+		sEmail = document.querySelector('#textEmail').value;
 
 
-function llenarTabla() {
-	var tbody = document.querySelector('#tblLibros tbody');
-	tbody.innerHTML='';
+		addCamposSistem(sIp,sNombre,sApellido,sDireccion,sEmail);
+impTablaCampos()
+}
+
+function impTablaCampos(){
+	
+		var list = JSON.parse(localStorage.getItem('newCamposset'));
+
+	// 	var tbody = document.querySelector('#tbCampos tbody');
+
+	// tbody.innerHTML='';
+
+	// for(var i=0; i<list.length; i++){
+	// 	var row = tbody.insertRow(i);
+	// 	var	ipCell = row.insertCell(0);
+	// 		ipCell.innerHTML = list[i].ip;
+
+	// 	tbody.appendChild(row);
+	//}
 
 
-	var aEmail = JSON.parse(localStorage.getItem('lemail')),
-		aPassword = JSON.parse(localStorage.getItem('lpassword')),
-		aAddress = JSON.parse(localStorage.getItem('laddress'));
+}
+
+ function get_ip(obj){
+            var obj_ip =  localStorage.setItem('textIp', JSON.stringify(obj.ip));          
+        }
+
+
+
+// llenarTabla();
+
+
+// function llenarTabla() {
+// 	var tbody = document.querySelector('#tblLibros tbody');
+// 	tbody.innerHTML='';
+
+
+// 	var aEmail = JSON.parse(localStorage.getItem('lemail')),
+// 		aPassword = JSON.parse(localStorage.getItem('lpassword')),
+// 		aAddress = JSON.parse(localStorage.getItem('laddress'));
 
 
 
 
-		var nCanDatos = aEmail.length;
+// 		var nCanDatos = aEmail.length;
 
-		for (var i = 0; i < nCanDatos; i++) {
+// 		for (var i = 0; i < nCanDatos; i++) {
 			
-			var fila = document.createElement('tr');
+// 			var fila = document.createElement('tr');
 
 
-			var celdaemail = document.createElement('td'),
-			 	celdapassword = document.createElement('td'),
-			 	celdaaddress = document.createElement('td');
+// 			var celdaemail = document.createElement('td'),
+// 			 	celdapassword = document.createElement('td'),
+// 			 	celdaaddress = document.createElement('td');
 
-			 var nodetextemail=document.createTextNode(aEmail[i]);
-			 	 nodetextpassword=document.createTextNode(aPassword[i]);
-			 	 nodetextaddress=document.createTextNode(aAddress[i]);
-
-
-			 	 celdaemail.appendChild(nodetextemail);
-			 	 celdapassword.appendChild(nodetextpassword);
-			 	 celdaaddress.appendChild(nodetextaddress);
-
-			 	 fila.appendChild(celdaemail);
-			 	 fila.appendChild(celdapassword);
-			 	 fila.appendChild(celdaaddress);
-
-			 	 tbody.appendChild(fila);
+// 			 var nodetextemail=document.createTextNode(aEmail[i]);
+// 			 	 nodetextpassword=document.createTextNode(aPassword[i]);
+// 			 	 nodetextaddress=document.createTextNode(aAddress[i]);
 
 
-		}
+// 			 	 celdaemail.appendChild(nodetextemail);
+// 			 	 celdapassword.appendChild(nodetextpassword);
+// 			 	 celdaaddress.appendChild(nodetextaddress);
 
-};
+// 			 	 fila.appendChild(celdaemail);
+// 			 	 fila.appendChild(celdapassword);
+// 			 	 fila.appendChild(celdaaddress);
+
+// 			 	 tbody.appendChild(fila);
+
+
+// 		}
+
+// };
 
