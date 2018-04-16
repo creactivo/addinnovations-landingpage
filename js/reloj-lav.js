9b31e78890f7c0bcb8114763410ba60aa26500e0
Arnaldo Castilla <arnaldo.castilla@hotmail.com>
2018-04-16 12:42:12 -0500

    reloj-lav

--- js/reloj-lav.js
+++ js/reloj-lav.js
@@ -1,21 +1,3 @@
-
-// (function(){
-// 	var contador = 0;
-// 	var saludo = function(){
-// 	contador++;
-// 	console.log(contador);
-	
-
-// 	if (contador === 5){
-// 		clearInterval(intervalo);
-// 	}
-
-// };
-
-// var intervalo = setInterval(saludo, 1000);
-// }())
-
-
 $(function(){
 	contenedor ()
 })
