a3f5085e9a3ea481e74ed29a33b6c37d7e00528e
Creactivo <zonacreactiva@gmail.com>
2018-04-17 19:23:20 -0500

    bueno esto va para arriba

--- controller/generalController.php
+++ controller/generalController.php
@@ -1,13 +1,37 @@
 <?php
+header('Access-Control-Allow-Origin: *'); 
 require_once $_SERVER['DOCUMENT_ROOT'].'/addinnovations-landingpage/model/general.php';
-/*if(isset($_POST['cerrarSesion']) && $_POST["cerrarSesion"]=="si"){
-	$general = new General();
-	$general->cerrarSesion();
-    return true;
-}*/
-
 $general = new General();
-$r = $general->prueba();
-var_dump($r);
+
+if(isset($_POST["token"]) && $_POST["token"]="generar"){
+	$token = $general->generarToken();
+	$r = $general->registrarToken($token);
+	echo json_encode($token);
+	return;
+}
+
+if(isset($_POST["trasa"]) && $_POST["trasa"]=="registrar"){
+	$dato = array(
+		"token_i" => $_POST["token_i"],
+		"id_diapo" => $_POST["id_diapo"],
+		"tiempo" => $_POST["tiempo"],
+		"ip" => $_POST["ip"],
+		"id_usuario" => $_POST["id_usuario"],
+	);
+	$trasa = $general->registrarDiapo($dato);
+	echo json_encode($trasa);
+	return;
+}
+
+if(isset($_POST["Usuario"]) && $_POST["Usuario"]=="registrarUser"){
+	$datos = array(
+		"nombre" => $_POST["nombre"],
+		"email" => $_POST["email"],
+		"token_r" => $_POST["token_r"],
+		"ip" => $_POST["ip"],
+	);
+	$registrarUsuario = $general->reistrarDatos($datos);
+	echo json_encode($registrarUsuario);return;
+}
 
 ?>
