2da756713d9d7c1e2e60aa31ef4ad52c166d3130
Arnaldo Castilla <arnaldo.castilla@hotmail.com>
2018-04-19 14:46:25 -0500

    funcionalidad formularios terminada

--- model/general.php
+++ model/general.php
@@ -135,6 +135,25 @@ class General extends Conexion{
         }
     }
 
+    public function actualizarUsuario($datos){
+        if(!empty($datos["telefono"])){
+            $sql = " update usuarios set empresa = ?, cargo = ?, telefono = ?, direccion = ? WHERE id = ?; ";
+            $dbh = $this->conexion->prepare($sql);
+            $dbh->bindParam(1, $datos["empresa"]);
+            $dbh->bindParam(2, $datos["cargo"]);
+            $dbh->bindParam(3, $datos["telefono"]);
+            $dbh->bindParam(4, $datos["direccion"]);
+            $dbh->bindParam(5, $datos["id"]);
+            if($dbh->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return "tel_req";
+        }
+    }
+
     public function getRealIP(){
 
         if (isset($_SERVER["HTTP_CLIENT_IP"]))
@@ -162,7 +181,60 @@ class General extends Conexion{
             return $_SERVER["REMOTE_ADDR"];
         }
         return rand(5, 'ABCDEFGHIJKLMOPQRSTUVXWYZ0123456789');//$_SERVER["HTTP_CLIENT_IP"];//$_SERVER["REMOTE_ADDR"];
-    } 
+    }
+
+    public function emailExiste($email){
+        $sql = " select email from usuarios where email = ? ";
+        $dbh = $this->conexion->prepare($sql);
+        $dbh->bindParam(1, $email);
+        $dbh->execute();
+        $num = $dbh->rowCount();
+        if($num==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function usuarioIntoCompl($datos){
+        //campos vacios
+        if(!empty($datos["nombre"]) && !empty($datos["email"]) && !empty($datos["telefono"]) ){
+            //validamos email
+            if(filter_var($datos["email"], FILTER_VALIDATE_EMAIL)){
+                if(self::emailExiste($datos["email"])){
+                    //insertamos usuarios
+                    $sql = " insert into usuarios (nombre, email, ip, empresa, cargo, telefono, direccion) values(?, ?, ?, ?, ?, ?, ?);  ";
+                    $stmt = $this->conexion->prepare($sql);
+                    $stmt->bindParam(1, $datos["nombre"]);
+                    $stmt->bindParam(2, $datos["email"]);
+                    $stmt->bindParam(3, $datos["ip"]);
+                    $stmt->bindParam(4, $datos["empresa"]);
+                    $stmt->bindParam(5, $datos["cargo"]);
+                    $stmt->bindParam(6, $datos["telefono"]);
+                    $stmt->bindParam(7, $datos["direccion"]);
+                    if($stmt->execute()){
+                        $id = $this->conexion->lastInsertId();
+                        //actualizar sesiones
+                        self::actualizarSesiones($id, $datos["token_re"]);
+                        //actualizar diapos
+                        self::actualizarDiapo($id, $datos["token_re"]);
+                        //retornar id para localstorage
+                        return $id;
+
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return 'email_regis';
+                }
+            }else{
+                return 'error_mail';
+            }
+        }else{
+            return 'nom_email_tel_req';
+        }
+
+    }
+
    
 }
 
